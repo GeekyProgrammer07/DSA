6aa78dee01e77860a68f218e3be93b43ca13a7de
GeekyProgrammer07 <sumansamantaiitkgp@gmail.com>
2025-06-22 16:54:53 +0530

    updated sum-of-dig

--- sum-of-digit-num.cpp
+++ sum-of-digit-num.cpp
@@ -8,7 +8,9 @@ int main() {
     cin >> n;
 
     while (n > 0) {
-        sum += n % 10;
+        if ((n % 10) % 2 != 0) {
+            sum += n % 10;
+        }
         n /= 10;
     }
     cout << "Sum: " << sum << endl;
